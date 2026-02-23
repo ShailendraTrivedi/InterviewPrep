5678f32a111227e9c51777783e66cda1a3157dad
Shailendra Trivedi <113613871+ShailendraTrivedi@users.noreply.github.com>
2026-02-23 21:49:07 +0530

    InterviewPrep

--- server/dist/config/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectDB = void 0;
-var db_1 = require("./db");
-Object.defineProperty(exports, "connectDB", { enumerable: true, get: function () { return db_1.connectDB; } });

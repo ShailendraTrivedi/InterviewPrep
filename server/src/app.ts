62379c83ba2870dc69e1c9b595ab8bc90e34e76b
Shailendra Trivedi <113613871+ShailendraTrivedi@users.noreply.github.com>
2026-03-21 16:41:03 -0700

    InterviewPrep

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,11 @@ import { globalExceptionHandler } from './exception/globalExceptionHandler';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 app.use(express.json());
 
 app.get('/api/health', (_req, res) => {
